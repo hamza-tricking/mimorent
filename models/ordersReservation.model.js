9dcd00ca5dd9372127ee66323d88b99f2bf6d292
Mohamed <hamzatricks@gmail.com>
2026-02-24 05:36:14 +0100

    hhhh

--- models/ordersReservation.model.js
+++ models/ordersReservation.model.js
@@ -29,13 +29,14 @@ const ordersReservationSchema = new mongoose.Schema({
         // Accept various Algerian formats:
         // 9 digits: 05XXXXXXXX, 06XXXXXXXX, 07XXXXXXXX
         // 8 digits: 5XXXXXXXX (without leading 0)
-        // 7 digits: 55XXXXX (shorter format)
+        // 7 digits: 55XXXXX or 05XXXXX (shorter format)
+        // 6 digits: 55XXXX or 05XXXX (very short)
+        // 5 digits: 55XXX or 05XXX (very short format)
         // 12 digits: +2135XXXXXXXX (with country code)
-        // 5-6 digits: very short formats for testing
         
-        return /^(0[5-7]\d{8}|[5-7]\d{7}|[5-7]\d{6}|[5-7]\d{5}|\+213[5-7]\d{8})$/.test(cleanNumber);
+        return /^(0[5-7]\d{8}|[5-7]\d{7}|0[5-7]\d{6}|[5-7]\d{6}|0[5-7]\d{5}|[5-7]\d{5}|\+213[5-7]\d{8})$/.test(cleanNumber);
       },
-      message: 'Phone number must be a valid Algerian number (e.g., 05XXXXXXXX, 55XXXXX, or +2135XXXXXXXX)'
+      message: 'Phone number must be a valid Algerian number (e.g., 05XXXXXXXX, 05XXXXX, or +2135XXXXXXXX)'
     }
   },
   

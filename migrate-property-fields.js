d5d3f940a420f0d74d585d6c802f6088e5084b3d
Mohamed <hamzatricks@gmail.com>
2026-03-12 06:31:09 +0100

    hello

--- migrate-property-fields.js
+++ migrate-property-fields.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Property = require('../models/property.model');
+const Property = require('./models/property.model');
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mimorent', {
